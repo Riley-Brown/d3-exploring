2842e50181d113f37a7f945d02d986190134448c
Riley-Brown <riley@riley.gg>
2019-05-27 19:20:35 -0400

    created scatterplot for life expectency based on births per capita

--- index.js
+++ index.js
@@ -15,3 +15,30 @@
 
 //   console.log(input);
 // });
+
+let width = 500;
+let height = 500;
+
+let yMax = d3.max(birthData2011, d => d.lifeExpectancy);
+let yMin = d3.min(birthData2011, d => d.lifeExpectancy);
+
+let yScale = d3
+  .scaleLinear()
+  .domain(d3.extent(birthData2011, d => d.lifeExpectancy))
+  .range([height, 0]);
+
+var xScale = d3
+  .scaleLinear()
+  .domain(d3.extent(birthData2011, d => d.births / d.population))
+  .range([0, width]);
+
+d3.select('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .selectAll('circle')
+  .data(birthData2011)
+  .enter()
+  .append('circle')
+  .attr('cx', d => xScale(d.births / d.population))
+  .attr('cy', d => yScale(d.lifeExpectancy))
+  .attr('r', 5);
